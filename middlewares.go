97346b898a91ca34291e7dabab77fb3410348f3a
Rogério Zambon <rogeriozambon@gmail.com>
2019-10-11 22:15:42 -0300

    adding an interface to combine middlewares

--- middlewares.go
+++ middlewares.go
@@ -1,9 +1,13 @@
 package middlewares
 
+import "net/http"
+
 const (
+	headerContentLength      = "Content-Length"
 	headerContentType        = "Content-Type"
 	headerContentTypeOptions = "X-Content-Type-Options"
 	headerFrameOptions       = "X-Frame-Options"
+	headerRemoteReferer      = "Referer"
 	headerXSSProtection      = "X-XSS-Protection"
 )
 
@@ -13,3 +17,30 @@ var htmlContentTypes = []string{
 	"application/xhtml",
 	"application/xhtml+xml",
 }
+
+var safeMethods = []string{
+	http.MethodGet,
+	http.MethodHead,
+	http.MethodOptions,
+	http.MethodTrace,
+}
+
+type Middleware interface {
+	Handler(http.HandlerFunc) http.HandlerFunc
+}
+
+func NewMiddleware(handler http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
+	for _, middleware := range middlewares {
+		handler = middleware.Handler(handler)
+	}
+	return handler
+}
+
+/*
+handler := NewMiddleware(
+	handler,
+	NewXSS("block", true),
+	NewFrameOptions("SAMEORIGIN"),
+	NewRemoteReferer([]string{http.MethodGet})
+)
+*/
